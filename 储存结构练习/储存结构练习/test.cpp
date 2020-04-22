c254cb2d9f1b8a0fe7ba8400523b4eae7f4c8bf3
Little-monster-HU <826398415@qq.com>
2020-04-22 23:08:41 +0800

    Create test.cpp

---
+++ 储存结构练习/储存结构练习/test.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#define N 100
+#include<assert.h>
+typedef int SeDataType;
+
+typedef struct SeqList
+{
+	SeDataType array[N];
+	size_t size;
+
+}SeqList;
+
+typedef struct SeqList
+{
+	SeDataType* array;
+	size_t size;
+	size_t capicity;
+}SeqList;
+
+
+
+void SeqListInit(SeqList ps)
+{
+	assert(ps);
+	ps->_array = (DataType*)malloc(sizeof(DataType)* 6);
+	if (ps->_array == NULL)
+	{
+		printf("初始化未成功!\n");
+		assert(0);
+		return;
+	}
+	ps->_capacity = 6;
+	ps->_size = 0;
+}
+int check_Capacity(SeqList ps)
+{
+	if (ps->_size == ps->_capacity)
+	{
+		DataType*ret = NULL;
+		//需要增容
+		//realloc(ps->_array,2*sizeof(DataType)*ps->_capacity);
+		ret = (DataType*)malloc(2 * sizeof(DataType)*(ps->_capacity));
+		if (ret == NULL)
+		{
+			printf("增容失败！\n");
+			assert(0);
+			return -1;
+		}
+		//ret=ps->_array;
+		memcpy(ret, ps->_array, sizeof(DataType)*ps->_size);
+		ps->_array = ret;
+		ps->_capacity = 2 * ps->_capacity;
+		printf("增容成功！\n");
+		return 1;
+	}
+	return 0;
+}
