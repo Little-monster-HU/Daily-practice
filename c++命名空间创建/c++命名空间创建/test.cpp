a60a06218e1ed4cb5a2b544b6c06399a1e641e4f
Little-monster-HU <826398415@qq.com>
2020-04-28 23:46:20 +0800

    Create test.cpp

---
+++ c++命名空间创建/c++命名空间创建/test.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<stdlib.h>
+namespace N
+{
+	int a = 10;
+	int b = 20;
+	int Add(int left, int right)
+	{
+		return left + right;
+	}
+	int Sub(int left, int right)
+	{
+		return left - right;
+	}
+}
+
+//using N::b;
+using namespace N;
+int main()
+{
+	printf("%d\n", N::a);
+	printf("%d\n", b);
+	Add(10, 20);
+	system("pause");
+	return 0;
+}
